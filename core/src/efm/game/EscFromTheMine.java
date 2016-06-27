1393d44a9f29413fcd1e8faddad7f8b6a51a1166
Kacper Jakobczak <kacper.jakobczak15@onet.pl>
2016-06-27 13:44:33 +0200

    Added new class of name GameObject. I need it to moving player and to do colisable walls

--- core/src/efm/game/EscFromTheMine.java
+++ core/src/efm/game/EscFromTheMine.java
@@ -12,7 +12,7 @@ public class EscFromTheMine extends ApplicationAdapter {
 
 	@Override
 	public void render () {
-		Gdx.gl.glClearColor(1, 0, 0, 1);
+		Gdx.gl.glClearColor(0, 0, 0, 1);
 		Gdx.gl.glClear(GL20.GL_COLOR_BUFFER_BIT);
 	}
 }
---
+++ core/src/efm/game/GameObject.java
@@ -0,0 +1,19 @@
+package efm.game;
+
+import com.badlogic.gdx.graphics.Texture;
+import com.badlogic.gdx.math.Rectangle;
+
+public class GameObject extends Rectangle {
+
+	public Texture texture;
+
+	public GameObject(Texture texture) {
+		this.texture = texture;
+
+	}
+
+	public Texture getTexture() {
+		return texture;
+	}
+
+}
