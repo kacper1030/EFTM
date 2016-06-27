612eb59cfa9cc17578c2494584d7a585eb0c3529
Kacper Jakobczak <kacper.jakobczak15@onet.pl>
2016-06-27 13:37:15 +0200

    Configured desktop launcher

--- desktop/src/efm/game/desktop/DesktopLauncher.java
+++ desktop/src/efm/game/desktop/DesktopLauncher.java
@@ -7,6 +7,13 @@ import efm.game.EscFromTheMine;
 public class DesktopLauncher {
 	public static void main (String[] arg) {
 		LwjglApplicationConfiguration config = new LwjglApplicationConfiguration();
+		
+		config.resizable = false;
+		config.width = 480;
+		config.height = 800;
+		
+		config.title = "ESC from the mine";
+		
 		new LwjglApplication(new EscFromTheMine(), config);
 	}
 }
