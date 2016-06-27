b4d990af2ecbe361e9ac7a73a1f4eee3d792f614
Kacper Jakobczak <kacper.jakobczak15@onet.pl>
2016-06-27 13:55:14 +0200

    Show it on screen

--- core/src/efm/game/EscFromTheMine.java
+++ core/src/efm/game/EscFromTheMine.java
@@ -3,16 +3,36 @@ package efm.game;
 import com.badlogic.gdx.ApplicationAdapter;
 import com.badlogic.gdx.Gdx;
 import com.badlogic.gdx.graphics.GL20;
+import com.badlogic.gdx.graphics.Texture;
+import com.badlogic.gdx.graphics.g2d.SpriteBatch;
 
 public class EscFromTheMine extends ApplicationAdapter {
 	
+	private Texture texPlayer;
+	private GameObject objPlayer;
+	private SpriteBatch sb;
+	
 	@Override
 	public void create () {
+		
+		sb = new SpriteBatch();
+		
+		texPlayer = new Texture("badlogic.jpg");
+		
+		objPlayer = new GameObject(texPlayer);
+		
 	}
 
 	@Override
 	public void render () {
 		Gdx.gl.glClearColor(0, 0, 0, 1);
 		Gdx.gl.glClear(GL20.GL_COLOR_BUFFER_BIT);
+		
+		sb.begin();
+		
+		sb.draw(objPlayer.getTexture(), 0, 0);
+		
+		sb.end();
+		
 	}
 }
